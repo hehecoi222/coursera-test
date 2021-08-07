9230dfe978ed351ca9492cf53956f3bf4d244c9d
hehecoi222 <hehecoi@outlook.com>
2021-08-07 17:37:03 +0700

    Learn about JS - Variables and Scope

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-var x ="Hello World"
+var message ="in Global"
+console.log("global: message:",message)
+function a() {
+    var message ="in A"
+    console.log("A: message:",message)
+    var b= function (){
+        console.log("B: message:",message)
+    }
+    b()
+}
+a()
