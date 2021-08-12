172b03fd92ed843392b265b60e741b206fc9f40a
hehecoi222 <hehecoi@outlook.com>
2021-08-12 18:27:33 +0700

    Good

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-function getsideDish(sideDish){
-    sideDish = sideDish || "Whaterver!"
-    console.log("Chicken with " + sideDish)
-}
+var company = new Object()
+company.name="Google"
+company.ceo = new Object()
+company.ceo.firstName="Sundar"
+company.ceo.favColor = "Color"
+console.log(company)
+console.log("the ceo of "+company.name + " has the first name that " + company.ceo.firstName)
 
-getsideDish("Noodles")
+//company.$stock of company = 110 Not work
+company["stock of company"] = 110
 
-getsideDish()
+console.log("stock of the comapny is "+ company["stock of company"])
+
+//You can even do this:
+
+stockPropName = "stock of company"
+
+company[stockPropName] = 220
+console.log("stock of the company is "+ company[stockPropName])
