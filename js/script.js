9a494e6293858f404356977f6a9ebe8ee592fe1c
hehecoi222 <hehecoi@outlook.com>
2021-08-12 18:35:25 +0700

    Way better to create Object()

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
-var company = new Object()
-company.name="Google"
-company.ceo = new Object()
-company.ceo.firstName="Sundar"
-company.ceo.favColor = "Color"
-console.log(company)
-console.log("the ceo of "+company.name + " has the first name that " + company.ceo.firstName)
+facebook = {
+    name: "Facebook",
+    ceo: {
+        firstName: "Mark",
+        favColor: "blue"
+    },
+    "stock of company": 110
+}
 
-//company.$stock of company = 110 Not work
-company["stock of company"] = 110
 
-console.log("stock of the comapny is "+ company["stock of company"])
-
-//You can even do this:
-
-stockPropName = "stock of company"
-
-company[stockPropName] = 220
-console.log("stock of the company is "+ company[stockPropName])
+console.log(facebook)
